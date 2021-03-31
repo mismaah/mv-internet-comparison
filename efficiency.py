b0531e85da1af21764b6d59cf3a3d6f0019923fa
Mismaah Abdulla <mismaahabdulla@gmail.com>
2021-03-31 15:24:32 +0500

    Add units to header

--- efficiency.py
+++ efficiency.py
@@ -4,10 +4,10 @@ import pandas
 
 df = pandas.read_csv("plans.csv")
 df = df.drop(df.columns[[5, 6]], axis=1)
-df["Price per GB"] = round(df["Price"] / df["Fair Usage"], 2)
+df["Price per GB"] = round(df["Price"] / df["Fair Usage (GB)"], 2)
 df = df.sort_values(by=["Price per GB"])
-df["Price per Mbps (Download)"] = round(df["Price"] / df["Download Speed"], 2)
-df["Price per Mbps (Upload)"] = round(df["Price"] / df["Upload Speed"], 2)
+df["Price per Mbps (Download)"] = round(df["Price"] / df["Download Speed (Mbps)"], 2)
+df["Price per Mbps (Upload)"] = round(df["Price"] / df["Upload Speed (Mbps)"], 2)
 
 print(df)
 df.to_csv("comparison.csv", index=False)
