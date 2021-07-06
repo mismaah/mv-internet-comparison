517affd4f4c940e52f0403a7393b85fd8a1aea54
Mismaah Abdulla <mismaahabdulla@gmail.com>
2021-07-06 10:22:42 +0500

    Comparison sorted by price/download speed

--- script.py
+++ script.py
@@ -7,13 +7,15 @@ df = df.drop(df.columns[[6, 7]], axis=1)
 df["Price per GB"] = (
     round(df["Price"] / df["Fair Usage (GB)"], 2) if df["Fair Usage (GB)"].any() else ""
 )
-df = df.sort_values(by=["Price per GB"])
 df["Price per Mbps (Download)"] = round(df["Price"] / df["Download Speed (Mbps)"], 2)
 df["Price per Mbps (Upload)"] = (
     round(df["Price"] / df["Upload Speed (Mbps)"], 2)
     if df["Upload Speed (Mbps)"].any()
     else ""
 )
+df = df.sort_values(by=["Price per GB"])
+# df = df.sort_values(by=["Price per Mbps (Download)"])
 
 print(df)
-df.to_csv("comparison.csv", index=False)
+df.to_csv("comparison[sorted-price-per-GB].csv", index=False)
+# df.to_csv("comparison[sorted-price-per-Mbps].csv", index=False)
